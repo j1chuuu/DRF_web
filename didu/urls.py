b66212af84ea0de895159c586f92f0414af992fb
JiSoo Hyeon <jichuuu0417@gmail.com>
2022-09-19 23:31:24 +0900

    authorserializer

--- didu/urls.py
+++ didu/urls.py
@@ -8,5 +8,6 @@ router.register('post', views.PostViewSet) # 2개 URL을 만들어줌
 
 
 urlpatterns = [
+    path('public/', views.PublicPostListAPIView.as_view()),
     path('', include(router.urls)),
 ]
--- didu/views.py
+++ didu/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
 
+
+class PublicPostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all() # filter(is_public=True)
+    serializer_class = PostSerializer
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
@@ -13,9 +18,10 @@ class PostViewSet(ModelViewSet):
     #     print("request.POST :", request.POST) # 실제 product에선 logger 사용하기
     #     return super().dispatch(request, *args, **kwargs)
 
-def post_list(request):
-    # 2개 분기
-    pass
+
+# def post_list(request):
+#     # 2개 분기
+#     pass
 
 
 def post_detail(request, pk):
