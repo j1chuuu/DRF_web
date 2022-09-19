012abe8b11e6777617aec05d6b332dd90be13095
JiSoo Hyeon <jichuuu0417@gmail.com>
2022-09-19 19:09:00 +0900

    serializer

--- didu/views.py
+++ didu/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        print()
+        return super().dispatch(request, *args, **kwargs)
+
+def post_list(request):
+    # 2개 분기
+    pass
+
+
+def post_detail(request, pk):
+    # request.method # => 3개 분기
+    pass
